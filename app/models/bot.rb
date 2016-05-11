290fd3bc4cdfc9218ec9ff6837647c6c039f7715
mbd-s <michaelbsilverberg@gmail.com>
2016-05-10 21:25:37 -0700

    added methods

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -3,4 +3,19 @@ class Bot < ActiveRecord::Base
     CLIENT.search(words, lang: "en").first.text
   end
 
+  def self.tweet status
+    CLIENT.update(status)
+  end
+
+  def self.follow_user name
+    CLIENT.follow(name)
+  end
+
+  def self.timeline
+    CLIENT.home_timeline
+  end
+
+  def self.find_tweet id
+    CLIENT.status(id)
+  end
 end
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,5 +1,6 @@
-CLIENT = Twitter::REST::Client.new do |config| 
+CLIENT = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['consumer_key']
   config.consumer_secret = ENV['consumer_secret']
   config.access_token = ENV['access_token']
-  config.access_token_secret = ENV['access_secret'] end
+  config.access_token_secret = ENV['access_secret']
+end
